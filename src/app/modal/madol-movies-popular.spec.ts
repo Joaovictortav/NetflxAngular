d2cf5e9c2c7d203ad092d87c4477fd0d330068cf
João Victor Tavares <joaojv497@hotmail.com>
2021-06-28 12:07:02 -0300

    segundo commit

--- src/app/modal/madol-movies-popular.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MadolMoviesPopular } from './madol-movies-popular';
-
-describe('MadolMoviesPopular', () => {
-  it('should create an instance', () => {
-    expect(new MadolMoviesPopular()).toBeTruthy();
-  });
-});
